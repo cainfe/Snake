cdeaf1b7fe01831158d5a8b83a7acbdc70cf4cff
cainfe <70218160+cainfe@users.noreply.github.com>
2024-06-15 10:36:03 -0500

    Snake can't reverse it's direction

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -15,10 +15,10 @@ public class Snake : MonoBehaviour
     }
 
     private void Update() {
-        if (Input.GetKeyDown(KeyCode.W)) { direction = Vector2.up; }
-        else if (Input.GetKeyDown(KeyCode.S)) { direction = Vector2.down; }
-        else if (Input.GetKeyDown(KeyCode.D)) { direction = Vector2.right; }
-        else if (Input.GetKeyDown(KeyCode.A)) { direction = Vector2.left; }
+        if (Input.GetKeyDown(KeyCode.W) && direction != Vector2.down) { direction = Vector2.up; }
+        else if (Input.GetKeyDown(KeyCode.S) && direction != Vector2.up) { direction = Vector2.down; }
+        else if (Input.GetKeyDown(KeyCode.D) && direction != Vector2.left) { direction = Vector2.right; }
+        else if (Input.GetKeyDown(KeyCode.A) && direction != Vector2.right) { direction = Vector2.left; }
     }
 
     private void FixedUpdate() {

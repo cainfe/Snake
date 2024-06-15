2fdb3b63eabd58a7eb84b6daf96e8243d01ecde8
cainfe <70218160+cainfe@users.noreply.github.com>
2024-06-15 09:26:30 -0500

    Snake Movement
    Created snake movement

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    private Vector2 direction = Vector2.right;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.W)) { direction = Vector2.up; }
+        else if (Input.GetKeyDown(KeyCode.S)) { direction = Vector2.down; }
+        else if (Input.GetKeyDown(KeyCode.D)) { direction = Vector2.right; }
+        else if (Input.GetKeyDown(KeyCode.A)) { direction = Vector2.left; }
+    }
+
+    private void FixedUpdate() {
+        this.transform.position = new Vector3(
+            Mathf.Round(this.transform.position.x + direction.x),
+            Mathf.Round(this.transform.position.y + direction.y),
+            0.0f
+            );
+    }
+}
